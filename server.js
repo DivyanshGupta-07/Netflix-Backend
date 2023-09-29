ef10b51d3205d7325b970e21a3eb86012c75c91f
Divyansh Gupta <divyanshg2000@gmail.com>
2023-09-29 11:54:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const userRoutes = require('./routes/UserRoutes');
+
+const app = express()
+const DB = process.env.DB;
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log('DB Connected')
+}).catch((err)=>{
+    console.log(err)
+})
+
+app.use('/api/user',userRoutes);
+
+app.listen(5000,console.log('server started'));
